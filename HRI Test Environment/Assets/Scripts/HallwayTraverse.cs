fed6229a840983b2652f31f7538eb6d2bb025481
Darren Tang <darrenvtang@gmail.com>
2020-10-23 10:20:11 -0400

    Remodeled Hallway scenario

---
+++ HRI Test Environment/Assets/Scripts/HallwayTraverse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HallwayTraverse : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public GameObject robot, leftArrow, rightArrow;
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ HRI Test Environment/Assets/Scripts/SimpleHallwayMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleHallwayMovement : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public GameObject robot, leftArrow;
+    public NavMeshAgent agent;
+    public int currWaypoint;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        leftArrow = GameObject.FindGameObjectWithTag("Left_Arrow");
+        robot = GameObject.FindGameObjectWithTag("Robot");
+    }
+    void Start()
+    {
+        Debug.Log(waypoints[0]);
+        Debug.Log(waypoints[1]);
+        Debug.Log(waypoints[2]);
+        Debug.Log(waypoints[3]);
+        currWaypoint = 0;
+        agent = robot.GetComponent<NavMeshAgent>();
+        agent.SetDestination(waypoints[0].transform.position);
+        leftArrow.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(currWaypoint);
+        if (Vector3.Distance(robot.transform.position, waypoints[currWaypoint].transform.position) < 0.5)
+        {
+            
+            agent.ResetPath();
+            currWaypoint++;
+            agent.SetDestination(waypoints[currWaypoint].transform.position);
+            if(currWaypoint == 1)
+            {
+                leftArrow.SetActive(true);
+            }
+            else
+            {
+                leftArrow.SetActive(false);
+            }
+
+        } 
+    }
+}
