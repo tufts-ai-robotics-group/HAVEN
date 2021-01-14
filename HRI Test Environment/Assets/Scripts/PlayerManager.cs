47d0b75fa382172324655fe9236d60d36794af1c
Darren Tang <darrenvtang@gmail.com>
2021-01-14 13:32:26 -0500

    Added module that records player position at set intervals and saves as JSON to external file, whos path and name are specified

---
+++ HRI Test Environment/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public string filename, path;
+    RecordMovement timeline;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        path += filename;
+        timeline = GameObject.FindGameObjectWithTag("Human").GetComponent<RecordMovement>();
+    }
+    void Start()
+    {
+        timeline.Begin();
+    }
+    void End()
+    {
+        using (FileStream file = new FileStream(path, FileMode.Append, FileAccess.Write))
+        {
+            using(StreamWriter sw = new StreamWriter(file))
+            {
+                sw.WriteLine(timeline.End());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Ended recording");
+            End();
+        }
+    }
+}
---
+++ HRI Test Environment/Assets/Scripts/RecordMovement.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable] 
+public class Snaps{
+    public float elapsed;
+    public Vector3 currPos;
+    public Quaternion currRot;
+    public Snaps(float _elapsed, Vector3 _currPos, Quaternion _currRot)
+    {
+        elapsed = _elapsed;
+        currPos = _currPos;
+        currRot = _currRot;
+    }
+    public override string ToString()
+    {
+        return "Elapsed: " + elapsed + ",Position: " + currPos + ",Rotation: " + currRot;
+    }
+}
+
+public class RecordMovement : MonoBehaviour
+{
+    private GameObject player;
+    private bool rec;
+    private float interval, elapsed;
+    Queue<Snaps> timelapse;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rec = false;
+        interval = 1f;
+        elapsed = 0f;
+        player = GameObject.FindGameObjectWithTag("Human");
+        timelapse = new Queue<Snaps>();
+    }
+    private void Start()
+    {
+        RecordStep();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rec)
+        {
+            if (Time.time > elapsed)
+            {
+                RecordStep();
+            }
+        }
+    }
+    public void Begin()
+    {
+        rec = true;
+    }
+    
+    public void RecordStep()
+    {
+        Snaps snap = new Snaps(elapsed, player.transform.position, player.transform.rotation);
+        timelapse.Enqueue(snap);
+        elapsed += interval;
+        Debug.Log("Recorded Step: " + snap.ToString());
+    }
+    public string End()
+    {
+        rec = false;
+        return Save();
+    }
+    public string Save()
+    {
+        Snaps[] timeline = timelapse.ToArray();
+        return JsonHelper.ToJson(timeline);
+    }
+}
